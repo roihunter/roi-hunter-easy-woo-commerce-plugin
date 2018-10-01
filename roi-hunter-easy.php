9f2f8dbdf8be1ecbb8ea79c7d19430bb6011e7b6
Karolína Vyskočilová <karolina@kybernaut.cz>
2018-10-01 18:24:07 +0200

    move settings to separate page

--- roi-hunter-easy.php
+++ roi-hunter-easy.php
@@ -55,15 +55,18 @@ function roi_hunter_easy_plugin_init() {
 	require_once( RH_EASY_DIR . 'includes/ajax.php' );
 	
 	// Admin
-	add_filter( 'woocommerce_get_settings_pages', 'roi_hunter_easy_wc_settings' );;
+	//add_filter( 'woocommerce_get_settings_pages', 'roi_hunter_easy_wc_settings' );;
+	require_once( RH_EASY_DIR . 'includes/admin/settings.php' );				
 	require_once( RH_EASY_DIR . 'includes/admin/plugin-settings-link.php' );				
 
 }
 
+/* TO REMOVE
 function roi_hunter_easy_wc_settings($settings) {
 	$settings[] = include( RH_EASY_DIR . 'includes/admin/wc-settings-tab.php' );
 	return $settings;
 }
+*/
 
 /**
  * The code that runs during plugin activation.
