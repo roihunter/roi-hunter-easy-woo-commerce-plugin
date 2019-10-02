5f57c6a05345a936eec850bb83b7b1493befc77c
Honza Liška <jan.liska@roihunter.com>
2019-10-02 15:34:41 +0200

    WIP [GSTV-719] almost ready, just needs to get polished

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -7,37 +7,39 @@ const getUrlParams = () => {
 
     if (goostavApplicationConfig.accessToken !== '') {
         return {
-            accessToken: goostavApplicationConfig.accessToken,
-            ...defaultParams
-        }
+            defaultParams,
+            additionalParams: {
+                accessToken: goostavApplicationConfig.accessToken,
+            }
+    }
     } else {
         return {
-            client_token: goostavApplicationConfig.clientToken,
-            website_url: goostavApplicationConfig.storeUrl,
-            release_platform: 'WOO_COMMERCE',
-            shop_id: window.location.hostname,
-            // shop_id: goostavApplicationConfig.customerId,
-            source_feed_url: goostavApplicationConfig.previewUrl,
-            source_feed_username: goostavApplicationConfig.wooCommerceApiKey,
-            source_feed_password: goostavApplicationConfig.wooCommerceApiSecret,
-            currency: goostavApplicationConfig.storeCurrency,
-            website_name: goostavApplicationConfig.storeName,
-            api_base_url: goostavApplicationConfig.rhStateApiBaseUrl,
-            language: goostavApplicationConfig.storeLanguage,
-            country_code: goostavApplicationConfig.storeCountry,
-            ...defaultParams
+            defaultParams,
+            additionalParams: {
+                client_token: goostavApplicationConfig.clientToken,
+                website_url: goostavApplicationConfig.storeUrl,
+                release_platform: 'WOO_COMMERCE',
+                shop_id: window.location.hostname,
+                // shop_id: goostavApplicationConfig.customerId,
+                source_feed_url: goostavApplicationConfig.previewUrl,
+                source_feed_username: goostavApplicationConfig.wooCommerceApiKey,
+                source_feed_password: goostavApplicationConfig.wooCommerceApiSecret,
+                currency: goostavApplicationConfig.storeCurrency,
+                website_name: goostavApplicationConfig.storeName,
+                api_base_url: goostavApplicationConfig.rhStateApiBaseUrl,
+                language: goostavApplicationConfig.storeLanguage,
+                country_code: goostavApplicationConfig.storeCountry,
+            }
         }
     }
 };
 
 const buildGoostavUrl = () => {
-    const urlBase = 'https://goostav-fe.roihunter.com/?';
-    const urlParams = getUrlParams();
-
-    console.log(urlParams);
-
-    const stringifiedParams = Object.keys(urlParams).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(urlParams[key])}`).join('&');
-    return urlBase + stringifiedParams;
+    const urlBase = 'https://goostav-fe.roihunter.com/?payload=';
+    const params = getUrlParams();
+    const defaultParams = Object.keys(params.defaultParams).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params.defaultParams[key])}`).join('&');
+    const additionalParams = JSON.stringify(params.additionalParams);
+    return urlBase + window.btoa(unescape(encodeURIComponent(additionalParams))) + '&' + defaultParams;
 };
 
 document.addEventListener("DOMContentLoaded", function() {
